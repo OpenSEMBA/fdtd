bd96645103aa8f8398031a17886457d916de8ea9
alberto <albergascon@gmail.com>
2024-07-09 13:16:41 +0200

    Minor in labels

--- src_json_parser/labels_mod.F90
+++ src_json_parser/labels_mod.F90
@@ -84,7 +84,7 @@ module labels_mod
    character (len=*), parameter :: J_MAT_TRANSFER_IMPEDANCE_NUMBER_POLES = "numberOfPoles"
 
    ! --  SPICE subcircuits
-   character (len=*), parameter :: J_SUBCIRCUITS  = "subCircuits"
+   character (len=*), parameter :: J_SUBCIRCUITS  = "subcircuits"
    character (len=*), parameter :: J_SUBCKT_NAME  = "name"
    character (len=*), parameter :: J_SUBCKT_PORTS = "numberOfPorts"
    character (len=*), parameter :: J_SUBCKT_FILE  = "file"
