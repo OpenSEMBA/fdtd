976742c23237b2ce4cb4ab44680298a3b754b96e
Luis Manuel Diaz Angulo <lm@diazangulo.com>
2024-12-24 22:52:49 +0100

    Reviewing full system tests

--- test/pyWrapper/test_full_system.py
+++ test/pyWrapper/test_full_system.py
@@ -2,14 +2,12 @@ from utils import *
 import pytest
 
 @pytest.mark.mtln
-def test_shielded_pair(tmp_path):
-    case = 'shieldedPair'
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'shielded_pair.exc')
-    makeCopy(tmp_path, CASE_FOLDER + case + '.fdtd.json')
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
-
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, flags = ['-mtlnwires'])
+def test_shieldedPair(tmp_path):
+    fn = CASE_FOLDER + 'shieldedPair/shieldedPair.fdtd.json'
+    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, \
+        flags = ['-mtlnwires'], run_in_folder=tmp_path)
     solver.run()
+    assert solver.hasFinishedSuccessfully() == True
     
     probe_files = ['shieldedPair.fdtd_wire_start_bundle_line_0_V_75_74_74.dat',
                    'shieldedPair.fdtd_wire_start_bundle_line_0_I_75_74_74.dat',
@@ -17,8 +15,6 @@ def test_shielded_pair(tmp_path):
                    'shieldedPair.fdtd_wire_end_Wz_75_71_74_s1.dat',
                    'shieldedPair.fdtd_wire_end_bundle_line_0_I_75_71_74.dat',
                    'shieldedPair.fdtd_wire_end_bundle_line_0_V_75_71_74.dat']
-    
-    assert solver.hasFinishedSuccessfully() == True
 
     p_expected = [Probe(OUTPUT_FOLDER+'shieldedPair.fdtd_wire_start_bundle_line_0_V_75_74_74.dat'),
                   Probe(OUTPUT_FOLDER+'shieldedPair.fdtd_wire_start_bundle_line_0_I_75_74_74.dat'),
@@ -36,19 +32,18 @@ def test_shielded_pair(tmp_path):
         assert np.allclose(p_expected[i].df.to_numpy()[:,0:4], p_solved.df.to_numpy()[:,0:4], rtol = 5e-2, atol=0.2)
 
 @pytest.mark.mtln
-def test_dielectric_antenna(tmp_path):
-    case = 'coated_antenna'
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'coated.exc')
-    makeCopy(tmp_path, CASE_FOLDER + case + '.fdtd.json')
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
+def test_coated_antenna(tmp_path):
+    fn = CASE_FOLDER + 'coated_antenna/coated_antenna.fdtd.json'
 
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, flags = ['-mtlnwires'])
+    solver = FDTD(\
+        input_filename = fn, path_to_exe=SEMBA_EXE, \
+        flags = ['-mtlnwires'], run_in_folder=tmp_path)
     solver.run()
+    assert solver.hasFinishedSuccessfully() == True
+    
     probe_current = solver.getSolvedProbeFilenames("mid_point_Wz")[0]
     probe_files = [probe_current]
     
-    assert solver.hasFinishedSuccessfully() == True
-
     p_expected = Probe(OUTPUT_FOLDER+'coated_antenna.fdtd_mid_point_Wz_11_11_11_s2.dat')
     
     p_solved = Probe(probe_files[0])
@@ -58,18 +53,15 @@ def test_dielectric_antenna(tmp_path):
     
 
 def test_holland(tmp_path):
-    case = 'holland1981'
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'holland.exc')
-    makeCopy(tmp_path, CASE_FOLDER + case + '.fdtd.json')
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
-
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, flags = [])
+    fn = CASE_FOLDER + 'holland/holland1981.fdtd.json'
+    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, run_in_folder=tmp_path)
+    
     solver.run()
+    assert solver.hasFinishedSuccessfully() == True
+    
     probe_current = solver.getSolvedProbeFilenames("mid_point_Wz")[0]
     probe_files = [probe_current]
     
-    assert solver.hasFinishedSuccessfully() == True
-
     p_expected = Probe(OUTPUT_FOLDER+'holland1981.fdtd_mid_point_Wz_11_11_12_s2.dat')
     
     p_solved = Probe(probe_files[0])
@@ -77,37 +69,32 @@ def test_holland(tmp_path):
 
 @pytest.mark.mtln
 def test_holland_mtln(tmp_path):
-    case = 'holland1981'
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'holland.exc')
-    makeCopy(tmp_path, CASE_FOLDER + case + '.fdtd.json')
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
-
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, flags = ['-mtlnwires'])
+    fn = CASE_FOLDER + 'holland/holland1981.fdtd.json'
+    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, flags = ['-mtlnwires'], run_in_folder=tmp_path)
+    
     solver.run()
+    assert solver.hasFinishedSuccessfully() == True
+    
     probe_current = solver.getSolvedProbeFilenames("mid_point_Wz")[0]
     probe_files = [probe_current]
     
-    assert solver.hasFinishedSuccessfully() == True
-
     p_expected = Probe(OUTPUT_FOLDER+'holland1981.fdtd_mid_point_Wz_11_11_12_s2.dat')
     
     p_solved = Probe(probe_files[0])
     assert np.allclose(p_expected.df.to_numpy()[:,0:3], p_solved.df.to_numpy()[:,0:3], rtol = 1e-5, atol=1e-6)
 
-def test_towelHanger(tmp_path):
-    case = 'towelHanger'
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'towelHanger.exc')
-    makeCopy(tmp_path, CASE_FOLDER + case + '.fdtd.json')
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
 
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE)
+def test_towelHanger(tmp_path):
+    fn = CASE_FOLDER + 'towelHanger/towelHanger.fdtd.json'
+    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE,\
+        run_in_folder=tmp_path)
     solver.run()
+    assert solver.hasFinishedSuccessfully() == True
+    
     probe_files = [solver.getSolvedProbeFilenames("wire_start")[0], 
                    solver.getSolvedProbeFilenames("wire_mid")[0], 
                    solver.getSolvedProbeFilenames("wire_end")[0]]
     
-    assert solver.hasFinishedSuccessfully() == True
-
     p_expected = [Probe(OUTPUT_FOLDER+'towelHanger.fdtd_wire_start_Wz_27_25_30_s1.dat'),
                   Probe(OUTPUT_FOLDER+'towelHanger.fdtd_wire_mid_Wx_35_25_32_s5.dat'),
                   Probe(OUTPUT_FOLDER+'towelHanger.fdtd_wire_end_Wz_43_25_30_s4.dat')]
@@ -118,19 +105,12 @@ def test_towelHanger(tmp_path):
    
     
 def test_sphere(tmp_path):    
-    case = 'sphere'
-    input_json = getCase(case)
-    input_json['general']['numberOfSteps'] = 20
-    input_json['probes'][0]['domain']['initialFrequency'] = 1e8
-    input_json['probes'][0]['domain']['finalFrequency'] = 1e9
-    
-    fn = tmp_path._str + '/' + case + '.fdtd.json'
-    with open(fn, 'w') as modified_json_file:
-        json.dump(input_json, modified_json_file) 
+    fn = CASE_FOLDER + 'sphere/sphere.fdtd.json'
+    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE, run_in_folder=tmp_path)
+    solver.input['general']['numberOfSteps'] = 20
+    solver.input['probes'][0]['domain']['initialFrequency'] = 1e8
+    solver.input['probes'][0]['domain']['finalFrequency'] = 1e9
 
-    makeCopy(tmp_path, EXCITATIONS_FOLDER+'gauss.exc')
-
-    solver = FDTD(input_filename = fn, path_to_exe=SEMBA_EXE)
     solver.run()
     assert solver.hasFinishedSuccessfully()
     
