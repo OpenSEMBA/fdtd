287b1d4e505fba3e9fa58346cf1b40985d1bdb62
adrianarce-elemwave <adrianarce@elemwave.com>
2025-02-17 13:58:33 +0100

    Refactor getCommandArgument

--- src_main_pub/getargs.F90
+++ src_main_pub/getargs.F90
@@ -12,31 +12,24 @@ contains
 
    subroutine getcommandargument(chain2,posic,argum,length,status)
       character (LEN=BUFSIZE)  ::  chain2, argum
-      character(LEN=1) :: a
       integer (kind=4)  :: length, status, posic, argumentStart, argumentEnd, endPathExecutableIndex
       integer (kind=4) :: i,j,n
-
-      !length is unused
-      !first remove all multiple blanks
-      CALL removeDoubleWhiteSpaces(chain2)
-
-      !remove binary path from string
-      !First, find the semba-fdtd list index
-      endPathExecutableIndex = index(chain2, sembaExecutableLabel) + len(sembaExecutableLabel)
 
       status=0
       argumentStart=0
       argumentEnd=0
+      n=1
+
+      CALL removeDoubleWhiteSpaces(chain2)
+      endPathExecutableIndex = index(chain2, sembaExecutableLabel) + len(sembaExecutableLabel)
 
       if (posic == 1) then
          argum=trim(adjustl(chain2(:endPathExecutableIndex)))
          return
       endif
+
       chain2=' '//trim(adjustl(chain2))//' '
-      n=1
-
-
-
+      
 
       findStart :  do i=endPathExecutableIndex  ,len(trim(adjustl(chain2)))+2
          if (chain2(i : i)==' ') n=n+1
@@ -48,15 +41,14 @@ contains
          endif
       end do findStart
 
-
       findEnd :  do i=argumentStart + 1 ,len(trim(adjustl(chain2)))+2
-         a = chain2(i : i)
          if (chain2(i : i)==' ') then
             argumentEnd=i-1
             continue
             exit findEnd
          endif
       end do findEnd
+
       if (argumentStart*argumentEnd==0) status=1
       argum=trim(adjustl(chain2(argumentStart : argumentEnd)))
 
@@ -72,23 +64,20 @@ contains
    function commandargumentcount(chain2)
       character (LEN=BUFSIZE)  ::  chain2
       integer (kind=4)  ::  status,n,i,j,commandargumentcount, endPathExecutableIndex
-
-      !!if (chain2(1 : 5)=='-----') then
-      !!    n=command_argument_count()
-      !!else
-      !length is unused
-      !first remove all multiple blanks
-      CALL removeDoubleWhiteSpaces(chain2)
-
-      endPathExecutableIndex = index(chain2, sembaExecutableLabel) + len(sembaExecutableLabel)
+      
       n=1
+      
+      CALL removeDoubleWhiteSpaces(chain2)
+      endPathExecutableIndex = index(chain2, sembaExecutableLabel) + len(sembaExecutableLabel)
+      
+
       do i=endPathExecutableIndex  ,len(trim(adjustl(chain2)))
          if (chain2(i : i)==' ') then
             n=n+1
          endif
       end do
+      
       status=0
-      !!!!!!endif
       commandargumentcount=n
       return
    end function
