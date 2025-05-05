c72f7ac7dcfd36fd6ac4fc143b35b9b56b9915e6
jajimesa <jajimesa@unirioja.es>
2025-05-05 20:16:49 +0200

    Hard reset

--- testData/cases/veritasium/closedCircuit_prepost.py
+++ testData/cases/veritasium/closedCircuit_prepost.py
@@ -2,12 +2,7 @@
 import numpy as np
 from numpy.fft import *
 import matplotlib.pyplot as plt
-import json
 import os
-from pathlib import Path
-import scipy.constants
-from skrf.media import Freespace
-from skrf.frequency import Frequency
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../', 'src_pyWrapper'))
@@ -16,18 +11,18 @@ SEMBA_EXE = '../../../build/bin/semba-fdtd'
 from pyWrapper import *
 
 # %%
-dt = 1e-12  # Time step
-tf = 150e-9  # Final time
+dt = 6e-10  # Time step
+tf = 6e-8  # Final time
 t = np.arange(0, tf, dt)
 
 # Create a Gaussian pulse
-t0 = 20e-9  # Center of the pulse
-sigma = 5e-9  # Width of the pulse
+t0 = 3e-8  # Center of the pulse
+sigma = 3e-8  # Width of the pulse
 v = 10.0 * np.exp(-(t - t0)**2 / (2 * sigma**2))
 
 plt.figure(figsize=(10, 6))
-plt.plot(t*1e9, v)
-plt.xlabel('Time (ns)')
+plt.plot(t, v)
+plt.xlabel('Time (s)')
 plt.ylabel('Voltage (V)')
 plt.title('Source Voltage')
 plt.grid(True)
@@ -49,8 +44,8 @@ solver.run()
 # resistance probe data
 resistance_current = Probe(solver.getSolvedProbeFilenames("resistance_current")[0])
 
-plt.plot(resistance_current['time']*1e9, resistance_current['current'], label='resistance')
-plt.xlabel('Time (ns)')
+plt.plot(resistance_current['time'], resistance_current['current'], label='resistance')
+plt.xlabel('Time (s)')
 plt.ylabel('Current (A)')
 plt.title('Current Signals')
 plt.legend()
@@ -58,3 +53,4 @@ plt.grid(True)
 
 plt.tight_layout()
 plt.show()
+# %%
