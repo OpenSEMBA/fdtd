029a092bbaf65cfe32069eeb15e40b1a1ec135b7
jajimesa <jajimesa@unirioja.es>
2025-05-06 19:03:19 +0200

    1/c effect detected

--- testData/cases/veritasium/closedCircuit_prepost.py
+++ testData/cases/veritasium/closedCircuit_prepost.py
@@ -11,6 +11,8 @@ SEMBA_EXE = '../../../build/bin/semba-fdtd'
 from pyWrapper import *
 
 # %%
+# GAUSSIAN PULSE
+
 dt = 6e-9  # Time step
 tf = 6e-7  # Final time
 t = np.arange(0, tf, dt)
@@ -32,7 +34,35 @@ plt.show()
 data = np.zeros((len(t), 2))
 data[:,0] = t
 data[:,1] = v
-np.savetxt('step.exc', data)
+np.savetxt('./step.exc', data)
+
+# %%
+# SIGMOID PULSE
+
+t = np.linspace(0, 5e-8, 1000)
+
+A = 10     
+k = 30e7
+t0 = 0.1e-8
+
+sigmoid_raw = 1 / (1 + np.exp(-k * (t - t0)))
+offset = 1 / (1 + np.exp(k * t0))
+scaling = 1 / (1 - offset)
+V = A * (sigmoid_raw - offset) * scaling
+
+plt.figure(figsize=(8, 4))
+plt.plot(t, V)
+plt.xlabel('t (s)')
+plt.ylabel('Voltage (V)')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Save excitation data
+data = np.zeros((len(t), 2))
+data[:,0] = t
+data[:,1] = V
+np.savetxt('./step.exc', data)
 
 # %%
 fn = 'closedCircuit.fdtd.json'
@@ -52,6 +82,7 @@ plt.ylabel('Corriente (A)')
 plt.title('Respuesta de la sonda del cable')
 plt.grid(True)
 plt.show()
+
 # %%
 data = np.loadtxt('closedCircuit.fdtd_source_current_Jy_109_150_99__110_150_100.dat', skiprows=1)
 time = data[:, 0]
@@ -74,6 +105,43 @@ current = data[:, 1]
 plt.plot(time, current)
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Corriente (A)')
-plt.title('Respuesta de la sonda del source')
+plt.title('Respuesta de la sonda del cube')
 plt.grid(True)
 plt.show()
+
+# %%
+data = np.loadtxt('closedCircuit.fdtd_cube_current_Jy_99_150_99__100_150_100.dat', skiprows=1)
+
+x1 = data[:, 0]
+y1 = data[:, 1]
+
+data = np.loadtxt('closedCircuit.fdtd_wire_current_Jx_105_99_99__105_100_100.dat', skiprows=1)
+
+x = data[:, 0]
+y = data[:, 1]
+
+data = np.loadtxt('closedCircuit.fdtd_source_current_Jy_109_150_99__110_150_100.dat', skiprows=1)
+
+x2 = data[:, 0]
+y2 = data[:, 1]
+
+data = np.loadtxt('closedCircuit.fdtd_cube_wave_current_Jx_100_149_99__100_150_100.dat', skiprows=1)
+
+x3 = data[:, 0]
+y3 = data[:, 1]
+
+plt.figure(figsize=(8, 6))
+#plt.plot(x1, -y1, color = 'blue', label = 'resistance wire')
+plt.plot(x, y, color = 'red',label = 'left wire' )
+plt.plot(x2, y2, color = 'green',label = 'source' )
+plt.plot(x3, -y3, color = 'blue', label = 'wave')
+plt.axvline(x=0.3e-8, color='orange', linestyle='-', label='1/c')
+plt.xlabel('time')
+plt.ylabel('Current')
+plt.grid(True)
+plt.legend()
+
+plt.xlim(right=1e-7)
+
+plt.show()
+# %%
