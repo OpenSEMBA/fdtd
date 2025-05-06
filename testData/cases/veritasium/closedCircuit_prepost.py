aaa4fab2eee590772c76962f6e18913df8062815
jajimesa <jajimesa@unirioja.es>
2025-05-06 15:38:35 +0200

    JSON works

--- testData/cases/veritasium/closedCircuit_prepost.py
+++ testData/cases/veritasium/closedCircuit_prepost.py
@@ -11,13 +11,13 @@ SEMBA_EXE = '../../../build/bin/semba-fdtd'
 from pyWrapper import *
 
 # %%
-dt = 6e-10  # Time step
-tf = 6e-8  # Final time
+dt = 6e-9  # Time step
+tf = 6e-7  # Final time
 t = np.arange(0, tf, dt)
 
 # Create a Gaussian pulse
-t0 = 3e-8  # Center of the pulse
-sigma = 3e-8  # Width of the pulse
+t0 = 3e-7  # Center of the pulse
+sigma = 3e-7  # Width of the pulse
 v = 10.0 * np.exp(-(t - t0)**2 / (2 * sigma**2))
 
 plt.figure(figsize=(10, 6))
@@ -36,10 +36,25 @@ np.savetxt('step.exc', data)
 
 # %%
 fn = 'closedCircuit.fdtd.json'
-solver = FDTD(input_filename=fn, path_to_exe=SEMBA_EXE)
+solver = FDTD(input_filename=fn, path_to_exe=SEMBA_EXE, flags=['-mapvtk'])
 solver.cleanUp()
 solver.run()
 
+# resistance probe data
+wire_current = Probe(solver.getSolvedProbeFilenames("wire_current")[0])
+
+plt.plot(wire_current['time'], wire_current['current'], label='wire')
+plt.xlabel('Time (s)')
+plt.ylabel('Current (A)')
+plt.title('Current Signals')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+# %%
+
+"""
 # %%
 # resistance probe data
 resistance_current = Probe(solver.getSolvedProbeFilenames("resistance_current")[0])
@@ -54,3 +69,4 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 # %%
+"""
