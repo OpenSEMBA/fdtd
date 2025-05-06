ac6d89d4bff309da0a69e903b897c9b1ca152985
jajimesa <jajimesa@unirioja.es>
2025-05-06 19:42:42 +0200

    Further promising changes

--- testData/cases/veritasium/closedCircuit_prepost.py
+++ testData/cases/veritasium/closedCircuit_prepost.py
@@ -71,45 +71,6 @@ solver.cleanUp()
 solver.run()
 
 # %%
-data = np.loadtxt('closedCircuit.fdtd_wire_current_Jx_105_99_99__105_100_100.dat', skiprows=1)
-time = data[:, 0]
-current = data[:, 1]
-
-# Graficar los datos
-plt.plot(time, current)
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Corriente (A)')
-plt.title('Respuesta de la sonda del cable')
-plt.grid(True)
-plt.show()
-
-# %%
-data = np.loadtxt('closedCircuit.fdtd_source_current_Jy_109_150_99__110_150_100.dat', skiprows=1)
-time = data[:, 0]
-current = data[:, 1]
-
-# Graficar los datos
-plt.plot(time, current)
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Corriente (A)')
-plt.title('Respuesta de la sonda del source')
-plt.grid(True)
-plt.show()
-
-# %%
-data = np.loadtxt('closedCircuit.fdtd_cube_current_Jy_99_150_99__100_150_100.dat', skiprows=1)
-time = data[:, 0]
-current = data[:, 1]
-
-# Graficar los datos
-plt.plot(time, current)
-plt.xlabel('Tiempo (s)')
-plt.ylabel('Corriente (A)')
-plt.title('Respuesta de la sonda del cube')
-plt.grid(True)
-plt.show()
-
-# %%
 data = np.loadtxt('closedCircuit.fdtd_cube_current_Jy_99_150_99__100_150_100.dat', skiprows=1)
 
 x1 = data[:, 0]
@@ -130,12 +91,17 @@ data = np.loadtxt('closedCircuit.fdtd_cube_wave_current_Jx_100_149_99__100_150_1
 x3 = data[:, 0]
 y3 = data[:, 1]
 
-plt.figure(figsize=(8, 6))
-#plt.plot(x1, -y1, color = 'blue', label = 'resistance wire')
-plt.plot(x, y, color = 'red',label = 'left wire' )
-plt.plot(x2, y2, color = 'green',label = 'source' )
-plt.plot(x3, -y3, color = 'blue', label = 'wave')
+plt.figure(figsize=(8, 9))
+
+plt.plot(x, y, color = 'red',label = 'left wire medition' )
+plt.plot(x2, y2, color = 'green',label = 'source medition' )
+
+#plt.plot(x1, -y1, color = 'blue', label = 'resistance medition')
+plt.plot(x3, -y3, color = 'blue', label = 'perpendicular resistance medition')
+
 plt.axvline(x=0.3e-8, color='orange', linestyle='-', label='1/c')
+plt.axvline(x=3.6e-8, color='orange', linestyle='-', label='11/c')
+
 plt.xlabel('time')
 plt.ylabel('Current')
 plt.grid(True)
