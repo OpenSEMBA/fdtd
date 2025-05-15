48308b860f3a7eacbc86a3cc0d08b2a4b5675e44
ashybabashyba <cramoss@unal.edu.co>
2025-05-15 12:05:01 +0200

    Update FDTD simulation parameters and excitation files for simple loop RC and R circuits. Added new theoretical current based on laplace transform

--- testData/cases/lumped_lines/simple_loop_R/simple_loop_prepost.py
+++ testData/cases/lumped_lines/simple_loop_R/simple_loop_prepost.py
@@ -2,6 +2,9 @@
 import numpy as np
 from numpy.fft import *
 import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from scipy import signal
+
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../', 'src_pyWrapper'))
@@ -9,11 +12,30 @@ SEMBA_EXE = '../../../../build/bin/semba-fdtd'
 
 from pyWrapper import *
 
+# %% Generate excitation and visualize
+def generateRampExcitation():
+    dt = 0.8e-12
+    t_final = 20e-9 
+    t = np.arange(0, t_final, dt)
+
+    f = np.zeros_like(t)
+
+    t1 = 1e-9
+    t2 = 5e-9
+
+    ramp_region = (t >= t1) & (t < t2)
+    f[ramp_region] = (t[ramp_region] - t1) / (t2 - t1)
+    f[t >= t2] = 1
+
+    # Guardar en archivo
+    data = np.column_stack((t, f))
+    np.savetxt('rampExcitation.exc', data)
+
 
 # %% Run solver
 fterminal = 'simple_loop_terminal.fdtd.json'
 flumped = 'simple_loop_lumped.fdtd.json'
-# generateGaussExcitation()
+generateRampExcitation()
 solver_terminal = FDTD(input_filename = fterminal, path_to_exe=SEMBA_EXE)
 solver_lumped = FDTD(input_filename = flumped, path_to_exe=SEMBA_EXE)
 solver_terminal.cleanUp()
@@ -23,35 +45,113 @@ solver_lumped.run()
 
 # %% Visualizing initial values of currents and voltages
 
-V_in = np.loadtxt("predefinedExcitation.1.exc", usecols=1)
-time = np.loadtxt("predefinedExcitation.1.exc", usecols=0)
+V_in = np.loadtxt("rampExcitation.exc", usecols=1)
+time = np.loadtxt("rampExcitation.exc", usecols=0)
 plt.figure()
 plt.plot(time, V_in, label='Initial excitation voltage')    
 plt.grid(which='both')
 plt.legend()
 plt.show()
 
-InitialBulk_probe = Probe(solver_terminal.getSolvedProbeFilenames("Initial current")[0])
-TerminalBulk_probe = Probe(solver_terminal.getSolvedProbeFilenames("Material current")[0])
-LumpedBulk_probe = Probe(solver_lumped.getSolvedProbeFilenames("Material current")[0])
+# %% Comparing initial current with theoretical
+
+I_teo = np.zeros_like(time)
+t1 = 1e-9
+t2 = 5e-9
+R = 50
+L = 1.65e-7
+
+ramp_region = (time >= t1) & (time < t2)
+I_teo[ramp_region] = (time[ramp_region] - t1) / (t2 - t1)/R  + (L/R**2)*(np.exp(-R*(time[ramp_region] - t1)/L) - 1)/(t2 - t1)
+I_teo[time >= t2] = 1/R + (L/R**2)*(np.exp(-R*(time[time >= t2] - t1)/L) - np.exp(-R*(time[time >= t2] - t2)/L))/(t2 - t1)
+
+# # Equivalent result for theoretical current but using scipy
+# num = [1]
+# den = [L, R]
+# system = signal.TransferFunction(num, den)
+# tout, I_out, _ = signal.lsim(system, U=V_in, T=time)
+
+InitialTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("Initial current")[0])
+InitialLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Initial current")[0])
+
+# # Calculation of the proper inductance of the system in case of R=0.
+
+# time_probe = InitialTerminal_probe['time'].to_numpy()
+# V_in_interp_func = interp1d(time, V_in, bounds_error=False, fill_value="extrapolate")
+# V_in_interp = V_in_interp_func(time_probe)  # Ahora está alineado con current
+# mask = (InitialTerminal_probe['time'] >= t1) & (InitialTerminal_probe['time'] <= t2)
+# current_slice = InitialTerminal_probe['current'][mask]
+# time_slice = InitialTerminal_probe['time'][mask]
+# vin_slice = V_in_interp[mask]
+# dI_dt = np.gradient(current_slice, time_slice)
+
+# with np.errstate(divide='ignore', invalid='ignore'):
+#     L_estimated = np.where(dI_dt != 0, vin_slice / dI_dt, np.nan)
+
+# L_mean = np.nanmean(L_estimated)
 
 plt.figure()
-plt.plot(InitialBulk_probe['time'].to_numpy(), InitialBulk_probe['current'].to_numpy(), label='Initial current on bulk', color='blue')
-plt.plot(time, V_in/1000, '--', label='Theoretical current with a 1000 ohms resistor', color='black')
+plt.plot(InitialTerminal_probe['time'].to_numpy(), InitialTerminal_probe['current'].to_numpy(), label='On terminal case', color='green')
+plt.plot(InitialLumped_probe['time'].to_numpy(), InitialLumped_probe['current'].to_numpy(), '--', label='On lumped case', color='red')
+plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
 plt.title('Initial current')
 plt.xlabel('Time')
 plt.ylabel('Current')
 plt.legend()
 plt.grid(which='both')
 
+#%% Comparison of currents between terminals and lumped
+StartTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("Start Material current")[0])
+StartLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Start Material current")[0])
+
+EndTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("End Material current")[0])
+EndLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("End Material current")[0])
+
+AfterLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("After Material current")[0])
+AfterTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("After Material current")[0])
+
+BeforeLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Before Material current")[0])
+BeforeTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("Before Material current")[0])
+
 plt.figure()
-plt.plot(TerminalBulk_probe['time'].to_numpy(), TerminalBulk_probe['current'].to_numpy(), label='Current on the circuit with a terminal', color='green')
-plt.plot(LumpedBulk_probe['time'].to_numpy(), LumpedBulk_probe['current'].to_numpy(), '--', label='Current on the with an equivalent lumped line', color='red')
+plt.plot(BeforeTerminal_probe['time'].to_numpy(), BeforeTerminal_probe['current'].to_numpy(), label='Before the terminal', color='green')
+plt.plot(BeforeLumped_probe['time'].to_numpy(), BeforeLumped_probe['current'].to_numpy(), '--', label='Before the lumped line', color='red')
 plt.title('Current on the circuit')
 plt.xlabel('Time')
 plt.legend()
 plt.grid(which='both')
+plt.tight_layout()
+plt.show()
 
+plt.figure()
+plt.plot(StartTerminal_probe['time'].to_numpy(), StartTerminal_probe['current'].to_numpy(), label='Start of the terminal cell', color='green')
+plt.plot(StartLumped_probe['time'].to_numpy(), StartLumped_probe['current'].to_numpy(), '--', label='Start of the lumped line cell', color='red')
+# plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
+plt.title('Current on the circuit')
+plt.xlabel('Time')
+plt.legend()
+plt.grid(which='both')
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+plt.plot(EndTerminal_probe['time'].to_numpy(), EndTerminal_probe['current'].to_numpy(), label='End of the terminal cell', color='green')
+plt.plot(EndLumped_probe['time'].to_numpy(), EndLumped_probe['current'].to_numpy(), '--', label='End of the lumped line cell', color='red')
+# plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
+plt.title('Current on the circuit')
+plt.xlabel('Time')
+plt.legend()
+plt.grid(which='both')
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+plt.plot(AfterTerminal_probe['time'].to_numpy(), AfterTerminal_probe['current'].to_numpy(), label='After the terminal', color='green')
+plt.plot(AfterLumped_probe['time'].to_numpy(), AfterLumped_probe['current'].to_numpy(), '--', label='After the lumped line', color='red')
+plt.title('Current on the circuit')
+plt.xlabel('Time')
+plt.legend()
+plt.grid(which='both')
 plt.tight_layout()
 plt.show()
 # %%
--- testData/cases/lumped_lines/simple_loop_RC/simple_loop_prepost.py
+++ testData/cases/lumped_lines/simple_loop_RC/simple_loop_prepost.py
@@ -2,6 +2,7 @@
 import numpy as np
 from numpy.fft import *
 import matplotlib.pyplot as plt
+from scipy import signal
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../', 'src_pyWrapper'))
@@ -9,45 +10,76 @@ SEMBA_EXE = '../../../../build/bin/semba-fdtd'
 
 from pyWrapper import *
 
+# %% Generate excitation and visualize
+def generateGaussExcitation():
+    dt = 0.8e-12
+    w0 = 1e-9 
+    t0 = 10*w0
+    t = np.arange(0, t0+20*w0, dt)
+    f = np.exp( -np.power(t-t0,2)/ w0**2 )
+
+    data = np.zeros((len(t), 2))
+    data[:,0] = t
+    data[:,1] = f
+    np.savetxt('predefinedExcitation.1.exc', data)
+
+# %% Generate excitation and visualize
+def generateRampExcitation():
+    dt = 0.8e-12
+    t_final = 2e-8 
+    t = np.arange(0, t_final, dt)
+
+    f = np.zeros_like(t)
+
+    t1 = 2e-9
+    t2 = 6e-9
+
+    ramp_region = (t >= t1) & (t < t2)
+    f[ramp_region] = (t[ramp_region] - t1) / (t2 - t1)
+    f[t >= t2] = 1
+
+    # Guardar en archivo
+    data = np.column_stack((t, f))
+    np.savetxt('rampExcitation.exc', data)
 
 # %% Run solver
-fterminal = 'simple_loop_terminal.fdtd.json'
+fterminal = 'current_bifurcation_RC.fdtd.json'
 flumped = 'simple_loop_lumped.fdtd.json'
 # generateGaussExcitation()
+generateRampExcitation()
 solver_terminal = FDTD(input_filename = fterminal, path_to_exe=SEMBA_EXE)
 solver_lumped = FDTD(input_filename = flumped, path_to_exe=SEMBA_EXE)
-solver_terminal.cleanUp()
+solver_lumped.cleanUp()
 
 solver_terminal.run()
 solver_lumped.run()
 
 # %% Visualizing initial values of currents and voltages
 
-V_in = np.loadtxt("predefinedExcitation.1.exc", usecols=1)
-time = np.loadtxt("predefinedExcitation.1.exc", usecols=0)
+V_in = np.loadtxt("rampExcitation.exc", usecols=1)
+time = np.loadtxt("rampExcitation.exc", usecols=0)
 plt.figure()
 plt.plot(time, V_in, label='Initial excitation voltage')    
 plt.grid(which='both')
 plt.legend()
 plt.show()
 
-InitialBulk_probe = Probe(solver_terminal.getSolvedProbeFilenames("Initial current")[0])
-TerminalBulk_probe = Probe(solver_terminal.getSolvedProbeFilenames("Material current")[0])
-LumpedBulk_probe = Probe(solver_lumped.getSolvedProbeFilenames("Material current")[0])
-InitialLumpedBulk_probe = Probe(solver_lumped.getSolvedProbeFilenames("Initial current")[0])
-
 #%%  Theoretical initial current
+InitialTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("Initial current")[0])
+InitialLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Initial current")[0])
 
-dt = time[1] - time[0]
-fq = fftfreq(len(time))/dt
+R = 1000
+C = 1e-12
+L = 1.65e-7
 
-V_in_f = fft(V_in)
-I_teo_f = V_in_f*(1/100 + 1j*2*np.pi*fq*1e-12)
-
-I_teo = ifft(I_teo_f)
+num = [R*C, 1]
+den = [L*R*C, L, R]
+system = signal.TransferFunction(num, den)
+tout, I_teo, _ = signal.lsim(system, U=V_in, T=time)
 
 plt.figure()
-plt.plot(InitialLumpedBulk_probe['time'].to_numpy(), InitialLumpedBulk_probe['current'].to_numpy(), label='Initial current on bulk', color='blue')
+# plt.plot(InitialTerminal_probe['time'].to_numpy(), InitialTerminal_probe['current'].to_numpy(), label='On terminal case', color='green')
+plt.plot(InitialLumped_probe['time'].to_numpy(), InitialLumped_probe['current'].to_numpy(), '--', label='On lumped case', color='red')
 plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
 plt.title('Initial current')
 plt.xlabel('Time')
@@ -56,16 +88,47 @@ plt.legend()
 plt.grid(which='both')
 
 #%% Comparison of currents between terminals and lumped
+StartLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Start Material current")[0])
+EndLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("End Material current")[0])
+
+AfterLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("After Material current")[0])
+AfterTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("After Material current")[0])
+
+BeforeLumped_probe = Probe(solver_lumped.getSolvedProbeFilenames("Before Material current")[0])
+BeforeTerminal_probe = Probe(solver_terminal.getSolvedProbeFilenames("Before Material current")[0])
 
 plt.figure()
-plt.plot(TerminalBulk_probe['time'].to_numpy(), TerminalBulk_probe['current'].to_numpy(), label='Current on the circuit with a terminal', color='green')
-plt.plot(LumpedBulk_probe['time'].to_numpy(), LumpedBulk_probe['current'].to_numpy(), '--', label='Current on the with an equivalent lumped line', color='red')
+# plt.plot(BeforeTerminal_probe['time'].to_numpy(), BeforeTerminal_probe['current'].to_numpy(), label='Before the terminal', color='green')
+plt.plot(BeforeLumped_probe['time'].to_numpy(), BeforeLumped_probe['current'].to_numpy(), '--', label='Before the lumped line', color='red')
+plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
 plt.title('Current on the circuit')
 plt.xlabel('Time')
-# plt.xlim((0, 2e-9))
 plt.legend()
 plt.grid(which='both')
-
 plt.tight_layout()
 plt.show()
+
+plt.figure()
+plt.plot(StartLumped_probe['time'].to_numpy(), StartLumped_probe['current'].to_numpy(), label='Start of the lumped line cell', color='blue')
+plt.plot(EndLumped_probe['time'].to_numpy(), EndLumped_probe['current'].to_numpy(), '--', label='End of the lumped line cell', color='red')
+plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
+plt.title('Current on the circuit')
+plt.xlabel('Time')
+plt.legend()
+plt.grid(which='both')
+plt.tight_layout()
+plt.show()
+
+plt.figure()
+# plt.plot(AfterTerminal_probe['time'].to_numpy(), AfterTerminal_probe['current'].to_numpy(), label='After the terminal', color='green')
+plt.plot(AfterLumped_probe['time'].to_numpy(), AfterLumped_probe['current'].to_numpy(), '--', label='After the lumped line', color='red')
+plt.plot(time, I_teo, '--', label='Theoretical current', color='black')
+plt.title('Current on the circuit')
+plt.xlabel('Time')
+plt.legend()
+plt.grid(which='both')
+plt.tight_layout()
+plt.show()
+
+
 # %%
