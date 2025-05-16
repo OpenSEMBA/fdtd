a630161bf75d527097d0a79b65fc7aef3a54d261
ashybabashyba <cramoss@unal.edu.co>
2025-05-16 12:26:18 +0200

    Corrected theoretical equation for the current to compare

--- testData/cases/lumped_lines/current_bifurcation/current_bifurcation_lumped_prepost.py
+++ testData/cases/lumped_lines/current_bifurcation/current_bifurcation_lumped_prepost.py
@@ -2,6 +2,7 @@
 import numpy as np
 from numpy.fft import *
 import matplotlib.pyplot as plt
+from scipy import signal
 
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../', 'src_pyWrapper'))
@@ -31,13 +32,25 @@ solver.run()
 
 # %% Visualizing initial values of currents and voltages
 
-V_in = np.loadtxt("predefinedExcitation.1.exc", usecols=1)
-time = np.loadtxt("predefinedExcitation.1.exc", usecols=0)
+V_in = np.loadtxt(solver["sources"][0]["magnitudeFile"], usecols=1)
+time = np.loadtxt(solver["sources"][0]["magnitudeFile"], usecols=0)
 plt.figure()
 plt.plot(time, V_in, label='Initial excitation voltage')    
 plt.grid(which='both')
 plt.legend()
 plt.show()
+
+# %% Comparing currents
+
+R_lumped = solver["materials"][1]["resistance"]
+R_terminal = solver["materials"][2]["terminations"][0]["resistance"]
+R = 1/(1/R_lumped + 1/R_terminal)  
+L = 1.65e-7
+
+num = [1]
+den = [L, R]
+system = signal.TransferFunction(num, den)
+tout, I_out, _ = signal.lsim(system, U=V_in, T=time)
 
 InitialBulk_probe = Probe(solver.getSolvedProbeFilenames("Bulk Initial probe")[0])
 TopBulk_probe = Probe(solver.getSolvedProbeFilenames("Bulk Top probe")[0])
@@ -45,7 +58,7 @@ BottomBulk_probe = Probe(solver.getSolvedProbeFilenames("Bulk Bottom probe")[0])
 
 plt.figure()
 plt.plot(InitialBulk_probe['time'].to_numpy(), InitialBulk_probe['current'].to_numpy(), label='Initial current on bulk', color='blue')
-plt.plot(time, V_in/500, '--', label='Theoretical current with two 1000 ohm resistors in parallel', color='black')
+plt.plot(tout, I_out, '--', label='Theoretical current with two resistors in parallel', color='black')
 plt.title('Initial current')
 plt.xlabel('Time')
 plt.ylabel('Current')
@@ -53,8 +66,8 @@ plt.legend()
 plt.grid(which='both')
 
 plt.figure()
-plt.plot(TopBulk_probe['time'].to_numpy(), TopBulk_probe['current'].to_numpy(), label='Current on resistor', color='green')
-plt.plot(BottomBulk_probe['time'].to_numpy(), BottomBulk_probe['current'].to_numpy(), '--', label='Current on lumped line', color='red')
+plt.plot(TopBulk_probe['time'].to_numpy(), TopBulk_probe['current'].to_numpy() + BottomBulk_probe['current'].to_numpy(), label='Sum of the lumped and terminal currents', color='green')
+plt.plot(tout, I_out, '--', label='Theoretical current with two resistors in parallel', color='black')
 plt.title('Current on the circuit')
 plt.xlabel('Time')
 plt.legend()
