f3236efab824feb350e3925c8b4047a001046ced
Alberto-o <albergascon@gmail.com>
2025-07-23 12:15:56 +0200

    fixes solver tests

--- test/system/system_testingTools.F90
+++ test/system/system_testingTools.F90
@@ -1,10 +1,35 @@
 module system_testingTools_mod
     use iso_c_binding
-    
+    use fdetypes
     implicit none
     
     character(len=*), parameter :: PATH_TO_TEST_DATA = 'testData/'
     character(len=*), parameter :: INPUT_EXAMPLES='input_examples/'
 
+contains
+    function initMedia(sgg, sggMiNo, sggMiEx, sggMiEy, sggMiEz, sggMiHx, sggMiHy, sggMiHz, sggMtag) result(res)
+        type(sggfdtdinfo), intent(in) :: sgg
+        type(media_matrices_t) :: res
+        integer (KIND=INTEGERSIZEOFMEDIAMATRICES), intent(in)   ::  &
+          sggMiNo(sgg%alloc(iHx)%XI : sgg%alloc(iHx)%XE,sgg%alloc(iHy)%YI : sgg%alloc(iHy)%YE,sgg%alloc(iHz)%ZI : sgg%alloc(iHz)%ZE), &
+          sggMiEx(sgg%alloc(iEx)%XI : sgg%alloc(iEx)%XE,sgg%alloc(iEx)%YI : sgg%alloc(iEx)%YE,sgg%alloc(iEx)%ZI : sgg%alloc(iEx)%ZE), &
+          sggMiEy(sgg%alloc(iEy)%XI : sgg%alloc(iEy)%XE,sgg%alloc(iEy)%YI : sgg%alloc(iEy)%YE,sgg%alloc(iEy)%ZI : sgg%alloc(iEy)%ZE), &
+          sggMiEz(sgg%alloc(iEz)%XI : sgg%alloc(iEz)%XE,sgg%alloc(iEz)%YI : sgg%alloc(iEz)%YE,sgg%alloc(iEz)%ZI : sgg%alloc(iEz)%ZE), &
+          sggMiHx(sgg%alloc(iHx)%XI : sgg%alloc(iHx)%XE,sgg%alloc(iHx)%YI : sgg%alloc(iHx)%YE,sgg%alloc(iHx)%ZI : sgg%alloc(iHx)%ZE), &
+          sggMiHy(sgg%alloc(iHy)%XI : sgg%alloc(iHy)%XE,sgg%alloc(iHy)%YI : sgg%alloc(iHy)%YE,sgg%alloc(iHy)%ZI : sgg%alloc(iHy)%ZE), &
+          sggMiHz(sgg%alloc(iHz)%XI : sgg%alloc(iHz)%XE,sgg%alloc(iHz)%YI : sgg%alloc(iHz)%YE,sgg%alloc(iHz)%ZI : sgg%alloc(iHz)%ZE)
+        integer (KIND=IKINDMTAG), intent(in)   ::  &
+          sggMtag(sgg%alloc(iHx)%XI : sgg%alloc(iHx)%XE,sgg%alloc(iHy)%YI : sgg%alloc(iHy)%YE,sgg%alloc(iHz)%ZI : sgg%alloc(iHz)%ZE)
+
+        res%sggMiNo = sggMiNo
+        res%sggMiEx = sggMiEx
+        res%sggMiEy = sggMiEy
+        res%sggMiEz = sggMiEz
+        res%sggMiHx = sggMiHx
+        res%sggMiHy = sggMiHy
+        res%sggMiHz = sggMiHz
+        res%sggMtag = sggMtag
+
+    end function
 
  end module system_testingTools_mod
--- test/system/test_init_solver.F90
+++ test/system/test_init_solver.F90
@@ -4,16 +4,15 @@ integer function test_init_solver() bind (C) result(err)
    implicit none
    type(semba_fdtd_t) :: semba
    type(solver_t) :: solver
-
+   type(media_matrices_t) :: media
    err = 0
    call chdir("./test/system/")
 
    call semba%init("-i init_solver.fdtd.json")
+   media = initMedia(semba%sgg,semba%sggMiNo,semba%sggMiEx,semba%sggMiEy,semba%sggMiEz,semba%sggMiHx,semba%sggMiHy,semba%sggMiHz,semba%sggMtag)
+
    call solver%init_control(semba%l, semba%maxSourceValue, semba%time_desdelanzamiento)
-   call solver%init(semba%sgg,semba%eps0, semba%mu0, semba%sggMiNo,& 
-                    semba%sggMiEx,semba%sggMiEy,semba%sggMiEz,& 
-                    semba%sggMiHx,semba%sggMiHy,semba%sggMiHz, & 
-                    semba%sggMtag, semba%SINPML_fullsize, semba%fullsize, semba%tag_numbers)
+   call solver%init(semba%sgg,semba%eps0, semba%mu0, media, semba%SINPML_fullsize, semba%fullsize, semba%tag_numbers)
    call solver%set_field_value(iEx, [2,4], [2,2], [2,2], 1.0)
    call solver%step(semba%sgg, semba%eps0, semba%mu0, semba%SINPML_FULLSIZE, semba%tag_numbers)
    if (solver%get_field_value(iHy, 2,2,2) == 0) err = err + 1
@@ -31,18 +30,18 @@ integer function test_rank_remapping() bind (C) result(err)
 
    type(semba_fdtd_t) :: semba
    type(solver_t) :: solver
+   type(media_matrices_t) :: media
 
    err = 0
    call chdir("./test/system/")
 
    call semba%init("-i init_solver.fdtd.json")
+   media = initMedia(semba%sgg,semba%sggMiNo,semba%sggMiEx,semba%sggMiEy,semba%sggMiEz,semba%sggMiHx,semba%sggMiHy,semba%sggMiHz,semba%sggMtag)
+   
    call solver%init_control(semba%l, semba%maxSourceValue, semba%time_desdelanzamiento)
-   call solver%init(semba%sgg,semba%eps0, semba%mu0, semba%sggMiNo,& 
-                    semba%sggMiEx,semba%sggMiEy,semba%sggMiEz,& 
-                    semba%sggMiHx,semba%sggMiHy,semba%sggMiHz, & 
-                    semba%sggMtag, semba%SINPML_fullsize, semba%fullsize, semba%tag_numbers)
+   call solver%init(semba%sgg,semba%eps0, semba%mu0, media, semba%SINPML_fullsize, semba%fullsize, semba%tag_numbers)
    call solver%set_field_value(iHy, [2,2], [2,2], [2,2], 1.0)
-   call solver%advanceEx(solver%sggMiEx)
+   call solver%advanceEx(solver%media%sggMiEx)
    if (solver%get_field_value(iEx, 2,2,2) /= -33.8822708) err = err + 1
    if (solver%get_field_value(iEx, 2,2,3) /= 33.8822708) err = err + 1
 
