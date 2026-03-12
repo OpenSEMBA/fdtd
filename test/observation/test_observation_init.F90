1cfda4a5542196ac1ae7c25c25e48472896fcb47
Luis Manuel Diaz Angulo <lmdiazangulo@ugr.es>
2026-03-12 06:22:14 +0000

    Aux Test files are now created in tmp/

--- test/observation/test_observation_init.F90
+++ test/observation/test_observation_init.F90
@@ -35,7 +35,7 @@ integer function test_init_time_movie_observation() bind(C) result(err)
     SINPML_fullsize = create_limit_t(0,4,0,4,0,4,3,3,3)
 
     facesNF2FF = create_facesNF2FF(.false., .false., .false., .false., .false., .false.)
-    control = create_control_flags(0, 0, 3, 10, "entryRoot", "wireflavour",&
+    control = create_control_flags(0, 0, 3, 10, "/tmp/entryRoot", "wireflavour",&
                                     .false., .false., .false., .false., .false.,&
                                     facesNF2FF)
 
