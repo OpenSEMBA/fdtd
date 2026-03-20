4e55526f9dbcce789824a16c2d5432c2f0ccc359
Alberto-o <albergascon@gmail.com>
2026-03-20 11:25:14 +0100

    add comparison interface for rkind

--- test/mtln/mtln_testingTools.F90
+++ test/mtln/mtln_testingTools.F90
@@ -135,6 +135,21 @@ contains
         
     end subroutine 
 
+    function checkNear_realkind(target, number, rel_tol) result(is_near)
+        real(kind=rkind), intent(in) :: target, number
+        real(kind=rkind) :: rel_tol
+        logical :: is_near
+        real :: abs_diff
+
+        abs_diff = abs(target-number)
+        if (abs_diff == 0.0) then
+            is_near = .true.
+        else 
+            is_near = abs(target-number)/target < rel_tol
+        endif
+
+    end function 
+
     function checkNear_real4(target, number, rel_tol) result(is_near)
         real, intent(in) :: target, number
         real :: rel_tol
