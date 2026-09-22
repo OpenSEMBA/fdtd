f46871e0407f111be89a6013c72e2ec2b86ee18f
Alberto-o <albergascon@gmail.com>
2026-09-22 15:21:56 +0200

    minor

--- src_mtln/preprocess.F90
+++ src_mtln/preprocess.F90
@@ -685,21 +685,19 @@ contains
     !
     ! ¡: start !: end
     ! [A] : V source w/series R OR I source w/parallel R
-    function writeSeriesRLCnode(node, termination, end) result(res)
+    function writeSeriesRLCnode(node, termination, end_node) result(res)
         type(nw_node_t), intent(in) :: node
         type(termination_t), intent(in) :: termination
-        character(len=*), intent(in) :: end
-        character(len=:), allocatable :: start
+        character(len=*), intent(in) :: end_node
         character(len=256), allocatable :: res(:)
         character(len=256) :: buff
-        start = trim(node%name)
         allocate(res(0))
-        call addResistance(res, start, start, start//"_R", termination%resistance)
-        call addInductance(res, start, start//"_R", start//"_L", termination%inductance)
+        call addResistance(res, node%name, node%name, node%name//"_R", termination%resistance)
+        call addInductance(res, node%name, node%name//"_R", node%name//"_L", termination%inductance)
         if (hasSource(termination)) then
-            call addSource(res, node%name, end, termination)
+            call addSource(res, node%name, end_node, termination)
         else
-            call addCapacitance(res, start, start//"_L", end, termination%capacitance)
+            call addCapacitance(res, node%name, node%name//"_L", end_node, termination%capacitance)
         end if
         call addTransmissionLineEquivalent(res, node)
         call addConductance(res, node)
@@ -861,7 +859,6 @@ contains
         else
             call addResistance(res,node%name, node%name, end_node, real(1e-10,rkind))
         end if
-        
         call addTransmissionLineEquivalent(res, node)
         call addConductance(res, node)
     end function
@@ -922,11 +919,9 @@ contains
         character(len=256), allocatable :: res(:)
         character(len=256) :: buff
         allocate(res(0))
-
         if (hasSource(termination)) then
             call addResistance(res, node%name, node%name, node%name//"_S", real(1e-10, rkind))
             call addSource(res, node%name, end_node, termination)
-
         else
             call addResistance(res, node%name, node%name, end_node, real(1e-10, rkind))
         end if
